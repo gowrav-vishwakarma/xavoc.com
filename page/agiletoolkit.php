3168d1851617506556664af8143063a4f8feae2d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2013-06-20 10:28:57 +0530

    type

--- page/agiletoolkit.php
+++ page/agiletoolkit.php
@@ -5,7 +5,7 @@ class page_agiletoolkit extends Page {
         parent::init();
 		
         $btn = $this->add('Button',null,'apply_entrance')->addClass('btn btn-primary btn-large')->set('Apply For Entrance Exam');
-        $btn->js('click')->univ()->errorMessage("Kindly meil at info@xavoc.com for now");
+        $btn->js('click')->univ()->errorMessage("Kindly email at info@xavoc.com for now");
     }
 
 	function defaultTemplate(){
